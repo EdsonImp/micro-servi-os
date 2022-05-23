c0a23bc3332c60dc9aeba5bf3da221c4a4b7cd93
edson silva <edsonnsousahotmail.com>
2022-05-23 11:53:14 -0300

    Criando projeto gateway zuul

--- hr-payroll/src/main/java/projeto/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/projeto/hrpayroll/HrPayrollApplication.java
@@ -2,11 +2,13 @@ package projeto.hrpayroll;
 
 import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //Nao precisa mais de ribom, o eukeca ja faz balanceamento de carga
 //@RibbonClient(name ="hr-work") //agora será cliente ribon, para balanceamento de requisiçoes
+@EnableCircuitBreaker //para o histryx, evitar falhas de timeout
 @EnableEurekaClient
 @EnableFeignClients //precisa ser add dependencia no pom.xml e depois essa anotation
 @SpringBootApplication
--- hr-payroll/src/main/java/projeto/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/projeto/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import projeto.hrpayroll.entities.Payment;
 import projeto.hrpayroll.services.PaymentService;
 
@@ -18,10 +20,17 @@ public class PaymentResource {
 	private PaymentService service;
 	
 	//a requisicao ficara assim http://localhost:8101/payments/1/days/10
+	@HystrixCommand(fallbackMethod ="getPaymentAlternativo" ) //do hystrix para criar alternativa a falha de timeout ou outras
 	@GetMapping(value="/{workerID}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerID, @PathVariable Integer days){
 		//agora instancia o pagamento pegando o metodo do service e os dados acima
 		Payment payment = service.getPayment(workerID, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	//Método alternativo para falhas no metodo que chama o hr-work
+	public ResponseEntity<Payment> getPaymentAlternativo(Long workerID, Integer days){
+		Payment payment = new Payment("Edson",200.00, days);
 		return ResponseEntity.ok(payment);
 	}
 
