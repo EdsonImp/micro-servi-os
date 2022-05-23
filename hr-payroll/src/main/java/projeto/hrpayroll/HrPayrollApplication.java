3656805c0710af14b44fe157c20742abb8b833a3
edson silva <edsonnsousahotmail.com>
2022-05-23 09:59:26 -0300

    Configuração eureka server

--- hr-payroll/src/main/java/projeto/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/projeto/hrpayroll/HrPayrollApplication.java
@@ -1,11 +1,13 @@
 package projeto.hrpayroll;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name ="hr-work") //agora será cliente ribon, para balanceamento de requisiçoes
+//Nao precisa mais de ribom, o eukeca ja faz balanceamento de carga
+//@RibbonClient(name ="hr-work") //agora será cliente ribon, para balanceamento de requisiçoes
+@EnableEurekaClient
 @EnableFeignClients //precisa ser add dependencia no pom.xml e depois essa anotation
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-work/src/main/java/projeto/hrwork/HrWorkApplication.java
+++ hr-work/src/main/java/projeto/hrwork/HrWorkApplication.java
@@ -1,8 +1,10 @@
 package projeto.hrwork;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkApplication {
 
