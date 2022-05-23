05d8df669badca50476fa717666aa6bc5f35622e
edson silva <edsonnsousahotmail.com>
2022-05-23 12:16:41 -0300

    Configurando rotas  zuul gateway

--- hr-api-gateway-zull/src/main/java/com/edson/hrapigatewayzull/HrApiGatewayZullApplication.java
+++ hr-api-gateway-zull/src/main/java/com/edson/hrapigatewayzull/HrApiGatewayZullApplication.java
@@ -2,7 +2,11 @@ package com.edson.hrapigatewayzull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy //habilita servidor zull
+@EnableEurekaClient // tamb´´me é cliente do eureka
 @SpringBootApplication
 public class HrApiGatewayZullApplication {
 
