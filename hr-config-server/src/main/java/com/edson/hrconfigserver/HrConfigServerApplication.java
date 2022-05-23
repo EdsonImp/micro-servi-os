a1358efc0586c1377a776cbb7f7df664756c37a6
edson silva <edsonnsousahotmail.com>
2022-05-23 15:22:30 -0300

    Configurando config server profile no github

--- hr-config-server/src/main/java/com/edson/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/edson/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.edson.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // habilita o config server
 @SpringBootApplication
 public class HrConfigServerApplication {
 
