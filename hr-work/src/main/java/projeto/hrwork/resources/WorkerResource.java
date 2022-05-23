dd99e182ba5958ac23391cfd8bc2a48b4306bde0
edson silva <edsonnsousahotmail.com>
2022-05-23 15:54:38 -0300

    Configurando hr-wwork como cliente do config server

--- hr-work/src/main/java/projeto/hrwork/resources/WorkerResource.java
+++ hr-work/src/main/java/projeto/hrwork/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +20,9 @@ import projeto.hrwork.repository.WorkRepository;
 @RequestMapping(value="/workers")
 public class WorkerResource {
 	
-	
+	@Value("${test.config}") //esse valor vem do git setado no properties do config
+	//acima, usado aki só pra testar se estar vindo algo do config
+	private String testConfig;
 	@Autowired
 	private Environment env;
 	//apenas par aimprimir coisas no log
@@ -36,6 +39,15 @@ public class WorkerResource {
 		Worker obj = repository.findById(id).get(); //retorna um optional, por isso o .get
 		return ResponseEntity.ok(obj);
 	} 
+	//abaixo uma requisicao para tesatar se esta pegando valor do config do git
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfig(){
+		logger.info("Config=" +testConfig);
+		return ResponseEntity.noContent().build();//retorna um corpo vazio
+	} 
+	
+	
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
 		List<Worker> list = repository.findAll();
